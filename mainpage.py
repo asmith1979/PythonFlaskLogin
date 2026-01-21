f31d9b6a9826dd42a776d4d7a66ab157f31d85c6
Andrew Smith <dancewalker79@hotmail.co.uk>
2026-01-21 14:31:33 +0000

    Added session for logging in and out

--- mainpage.py
+++ mainpage.py
@@ -1,26 +1,18 @@
 # Script: mainpage.py
 # Author: Andrew Smith
 # Date: September 2024
-# Description: Main landing page
+# Description: Main landing page (route handling)
 
-from flask import Flask, render_template, request, redirect, url_for
-import hashlib
-
-userData = []
-
-# Add User Data for Demonstration purpose
-userData.append([1,"mel.gibson@mycompany.com", "b1de3dec47fd10a41669145570df961d", "Gibson", "Mel", "Mr", "077892456789", 1, "03/23/2025", 1, 1, 1, 1, "null"])
-userData.append([2,"sly.stallone@mycompany.com", "b9eb312c6b776285dab43253df7c2c40", "Stallone", "Sly", "Mr", "079945646664", 1, "03/23/2025", 1, 1, 1, 1, "null"])
-userData.append([3,"nicole.kidman@mycompany.com", "9c1d10fff0a08054dc4c808cd5ae8223", "Kidman", "Nicole", "Miss", "0749665223", 0, "03/23/2025", 1, 1, 0, 0, "null"])
-userData.append([4,"angelina.jolie@mycompany.com", "23915be13492eec1f39eacf8456574fc", "Jolie", "Angelina", "Miss", "0771233333", 0, "03/23/2025", 1, 1, 0, 0, "null"])
-userData.append([5,"john.travolta@mycompany.com", "0aaca691d1c853b17178c0ddda077796", "Travolta", "John", "Mr", "07749888999", 0, "03/23/2025", 1, 1, 0, 1, "null"])
-userData.append([6,"sandra.bullock@mycompany.com", "ff8062c7a0ceee303cf593309fca1b6b", "Bullock", "Sandra", "Miss", "079945512", 0, "03/23/2025", 1, 1, 0, 1, "null"])
-userData.append([7,"harrison.ford@mycompany.com", "b0dfcdfb749fb18159e2d61ead5f5e14", "Ford", "Harrison", "Mr", "07723478888", 1, "03/23/2025", 1, 1, 1, 1, "null"])
-userData.append([8,"ben.stiller@mycompany.com", "9bb374323c1860b999a403150d5de424", "Stiller", "Ben", "Mr", "079487789777", 0, "03/23/2025", 1, 1, 0, 1, "null"])
-userData.append([9,"chris.pratt@mycompany.com", "4f76aedd9220999a5731885ddcf51733", "Pratt", "Chris", "Mr", "0773574199", 0, "03/23/2025", 1, 1, 0, 1, "null"])
-userData.append([10,"adam.sandler@mycompany.com", "b53116f942177afdf4bfa41fb87ebc6c", "Sandler", "Adam", "Mr", "07900886677", 0, "03/23/2025", 1, 1, 0, 1, "null"])
+from flask import Flask, render_template, request, redirect, url_for, session
+import useradmin as user_admin
 
 app = Flask(__name__)
+app.secret_key = "super-secret-key"  # required for sessions
+
+@app.route("/logout", methods=["GET", "POST"])
+def logout():
+    session.pop('useremail', None)
+    return redirect(url_for('login'))
 
 # Route for handling the login page logic
 @app.route('/login', methods=['GET', 'POST'])
@@ -33,53 +25,48 @@ def login():
         # getting input with name = lname in HTML form 
         userPassword = request.form.get("password")    
         
+        session['useremail'] = request.form['useremail']
+        print(dict(session))
+        
         # Check for invalid login 
-        if authoriseUserLogin(userName,userPassword) == False:
+        if user_admin.authoriseUserLogin(userName,userPassword) == False:
             msg = 'Invalid Login Details'
-            render_template('login.html', msg=msg)
+            session.pop('useremail', None)
+            return render_template('login.html', msg=msg)
         else:
             msg = ""
             return render_template('dashboard.html', msg=msg)
 
     return render_template('login.html', msg=msg)
 
-# A function used to authorise user login     
-def authoriseUserLogin(userIn, passwordIn):
-    # Make user data accessible
-    global userData
-    authorisedLogin=False 
-    indexCounter = 0
-    recordFound = -1 # Identifies if record found
-    
-    # Get the MD5 hash equivelant....
-    
-    encryptedResult = hashlib.md5(passwordIn.encode())    
-    encUserPassword = encryptedResult.hexdigest()
-    
-    # Get the user password if it exists in the data 
-    while indexCounter < len(userData):
-        if userData[indexCounter][1] == userIn:
-            recordFound = indexCounter
-        indexCounter = indexCounter + 1
-        
-    if recordFound > -1:
-        # Get the password stored in the data 
-        storedEncryptedPassword = userData[recordFound][2]
-        
-        if encUserPassword == storedEncryptedPassword:
-            authorisedLogin = True
-
-    return authorisedLogin
-
 # Forgot password     
 @app.route("/forgotpassword", methods =["GET", "POST"])
 def forgotPassword():
-    return render_template('forgotpass.html')
+    msg = ""
+    if request.method == 'POST':
+        
+        # Get the email address that has been entered to send the new password to 
+        userEmailAddress = request.form.get("useremail")
+        
+        if user_admin.forgotPassword(userEmailAddress) == "":
+            msg = "No Email Address Found"
+            return render_template('forgotpass.html', msg=msg)
+        else:
+            msg = "Email Address Found"
+            return render_template('forgotpass.html', msg=msg)
+    
+    return render_template('forgotpass.html', msg=msg)
     
 # Dashboard app     
 @app.route("/dashboard", methods =["GET", "POST"])
 def dashboard():
-    return render_template('dashboard.html')
+    if 'useremail' in session:
+        msg = ("You are logged in as " + session['useremail'])
+        return render_template('dashboard.html', msg=msg)
+    else:
+        msg = 'You need to login'
+        # return render_template('login.html', msg=msg)
+        return redirect(url_for('login'))
     
 if __name__ == '__main__':
     app.run()
--- useradmin.py
+++ useradmin.py
@@ -4,6 +4,30 @@
 # Description: User Administration back-end functionality
 
 import random
+import hashlib
+from datetime import datetime 
+
+##################################
+###  Back-end User Data Store  ###
+##################################
+
+userData = []
+
+# Add User Data for Demonstration purpose
+userData.append([1,"mel.gibson@mycompany.com", "b1de3dec47fd10a41669145570df961d", "Gibson", "Mel", "Mr", "077892456789", True, "03/23/2025", "SUPERADMIN"])
+userData.append([2,"sly.stallone@mycompany.com", "b9eb312c6b776285dab43253df7c2c40", "Stallone", "Sly", "Mr", "079945646664", True, "03/23/2025", "SUPERADMIN"])
+userData.append([3,"nicole.kidman@mycompany.com", "9c1d10fff0a08054dc4c808cd5ae8223", "Kidman", "Nicole", "Miss", "0749665223", False, "03/23/2025", "ADMIN"])
+userData.append([4,"angelina.jolie@mycompany.com", "23915be13492eec1f39eacf8456574fc", "Jolie", "Angelina", "Miss", "0771233333", False, "03/23/2025", "ADMIN"])
+userData.append([5,"john.travolta@mycompany.com", "0aaca691d1c853b17178c0ddda077796", "Travolta", "John", "Mr", "07749888999", False, "03/23/2025", "ADMIN"])
+userData.append([6,"sandra.bullock@mycompany.com", "ff8062c7a0ceee303cf593309fca1b6b", "Bullock", "Sandra", "Miss", "079945512", False, "03/23/2025", "ADMIN"])
+userData.append([7,"harrison.ford@mycompany.com", "b0dfcdfb749fb18159e2d61ead5f5e14", "Ford", "Harrison", "Mr", "07723478888", True, "03/23/2025", "STANDARD"])
+userData.append([8,"ben.stiller@mycompany.com", "9bb374323c1860b999a403150d5de424", "Stiller", "Ben", "Mr", "079487789777", False, "03/23/2025", "STANDARD"])
+userData.append([9,"chris.pratt@mycompany.com", "4f76aedd9220999a5731885ddcf51733", "Pratt", "Chris", "Mr", "0773574199", False, "03/23/2025", "STANDARD"])
+userData.append([10,"adam.sandler@mycompany.com", "b53116f942177afdf4bfa41fb87ebc6c", "Sandler", "Adam", "Mr", "07900886677", False, "03/23/2025", "STANDARD"])
+
+##############################################
+###  Private Back-end admin functionality  ###
+##############################################
 
 # Function: generatePassword
 # Description: Generates a secure password
@@ -50,3 +74,94 @@ def generatePassword():
     genPassword=(firstCharacter+secondCharacter+thirdCharacter+fourthCharacter+fifthCharacter+sixthCharacter+seventhCharacter+eigthCharacter)
     
     return genPassword
+    
+# A function used to authorise user login     
+def authoriseUserLogin(userIn, passwordIn):
+    # Make user data accessible
+    global userData
+    authorisedLogin=False 
+    indexCounter = 0
+    recordFound = -1 # Identifies if record found
+    
+    # Get the MD5 hash equivelant....
+    
+    encryptedResult = hashlib.md5(passwordIn.encode())    
+    encUserPassword = encryptedResult.hexdigest()
+    
+    # Get the user password if it exists in the data 
+    while indexCounter < len(userData):
+        if userData[indexCounter][1] == userIn:
+            recordFound = indexCounter
+        indexCounter = indexCounter + 1
+        
+    if recordFound > -1:
+        # Get the password stored in the user data 
+        storedEncryptedPassword = userData[recordFound][2]
+        
+        if encUserPassword == storedEncryptedPassword:
+            authorisedLogin = True
+
+    return authorisedLogin
+    
+def getUserEmailAddress(emailAddressIn):
+    global userData
+    # A blank email address represents a null value - no email address found 
+    emailAddressReturn = ""
+    
+    recordNo = 0
+    dataStoreLength = len(userData)
+    
+    while recordNo < (dataStoreLength-1):
+        if userData[recordNo][1] == emailAddressIn:
+            emailAddressReturn = userData[recordNo][1]
+            break    
+            
+        recordNo = recordNo + 1
+    
+    return emailAddressReturn 
+    
+    
+
+def forgotPassword(emailAddressIn):
+    # Detect if email address is in the data store to send
+    emailAddr = getUserEmailAddress(emailAddressIn)
+    
+    return emailAddr
+
+################################################
+###  Administration Dashboard Functionality  ###
+################################################
+
+# Function Name: addUserToDataStore
+# Function Description: A function used to add a user to the user data store collection
+# Function Parameters (input): email address, surname, firstname, title, phone number, active state, user account type 
+def addUserToDataStore(emailAddressIn, surnameIn, firstnameIn, titleIn, phoneNoIn, activeIn, userAccountTypeIn):
+
+    ##################################
+    ###  Create Automated Details  ###
+    ##################################
+    
+    # Get / Create new User ID
+    newUserID = userData[(len(userData)-1)][0]
+    # print("New User ID: " + str(newUserID))    
+
+    # Get new user password 
+    newUserPassword = generatePassword()    
+    
+    # Encrypt new user password to be stored in the data store in encrypted format 
+    encryptedResult = hashlib.md5(newUserPassword.encode())
+    encryptedPassword = encryptedResult.hexdigest()
+    
+    # print("New Password: " + str(encryptedPassword))
+    
+    # Get Formatted creation date 
+    nowDateTime = datetime.now()
+    formattedNowDateTime = nowDateTime.strftime("%Y-%m-%d %H:%M:%S")
+    
+    # print("Date Time: " + formattedNowDateTime)
+    
+    # Add New User details to the user data store 
+    userData.append([newUserID, emailAddressIn, encryptedPassword, surnameIn, firstnameIn, titleIn, phoneNoIn, activeIn, formattedNowDateTime, userAccountTypeIn])
+    
+    
+
